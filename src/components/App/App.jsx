2b40b8c3ad2c0a58e0800855dcbb8703caa06d73
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-03-22 01:15:44 +0300

    Galanin_Pavel : Added GH API fetch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,17 @@
 function App() {
     console.log(process.env);
+
+    let url = `https://api.github.com/users/Pavel-Innokentevich-Galanin`;
+
+    fetch(url, {
+        "method": "GET",
+        "headers": {
+            "Authorization": `Token ${process.env.REACT_APP__GH_token}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
+
     return (
         <div className="App">
             Hello, world!
